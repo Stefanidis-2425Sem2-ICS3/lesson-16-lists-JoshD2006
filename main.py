9bd3710c8ba7b0bf7cde613b4f03a0ca650ccefc
JoshD2006 <dinisj@students.dsbn,org>
2025-03-24 12:06:21 -0400

    Final 1

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+# Joshua Dinis
+# Randomly Generated List Assignment
+# March 24th 2025
+# generates a list of 100 variables, and then calculates the average
 
+import random
+
+randNums = []
+
+for index in range (100):
+    randNums.append(random.randint(1,100))
+
+avg = (sum(randNums))/100    
+avgStr = str(avg)
+randNumsStr = str(randNums)
+print ("The list of 100 randomly generated numbers is: \n" + randNumsStr)
+
+print ("The average of the list of numbers is:  " + avgStr)
